9f309dd310f78e542c6c45c5528b6fb49ee35ad2
J. Felipe <joaofeliperochads@gmail.com>
2024-04-11 22:51:43 -0300

    Integração com o banco de dados.

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
 
-app = Flask("__name__")
-
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    soil_humidity = db.Column(db.Float, nullable=False)
+    ambient_humidity = db.Column(db.Float, nullable=False)
+    ambient_temperature = db.Column(db.Float, nullable=False)
+    water_volume = db.Column(db.Float, nullable=False)
 
 @app.route("/")
 def home():
     return render_template('index.html')
-
 
 @app.route("/show-data")
 def show_data():
@@ -25,5 +34,6 @@ def delete_data():
 def statistics():
     return render_template('statistics.html')
 
-
-
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
